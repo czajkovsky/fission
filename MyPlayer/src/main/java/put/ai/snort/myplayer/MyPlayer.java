802a8161de0c579adbaed1623f0586d12a3c43a9
agaaa <agnieszka.czechumska@gmail.com>
2014-01-08 16:10:38 +0100

    alpha-beta

--- MyPlayer/src/main/java/put/ai/snort/myplayer/MyPlayer.java
+++ MyPlayer/src/main/java/put/ai/snort/myplayer/MyPlayer.java
@@ -13,165 +13,186 @@ import put.ai.snort.game.Move;
 import put.ai.snort.game.Player;
 
 public class MyPlayer extends Player {
-	
-	private Random random=new Random(0xdeadbeef);
-	
-    @Override
-    public String getName() {
-        return "min max";
-    }
 
-    public static void main(String args[]) {	
-    }
-    
-    @Override
-    public Move nextMove(Board b) {
-        
-    	//lista dostepnych ruchow
-    	List<Move> moves = b.getMovesFor(getColor());
+	private Random random = new Random(0xdeadbeef);
 
-    	//szukanie ruchu o maksymalnej wartosci funkcji oceny wsrod ruchow gracza min
-    	int level = 2, //ilosc iteracji
-    		tmp, 
-    		bestValue = minMove(moves.get(0), b, level);
+	private List <Integer> values;
+	
+	public MyPlayer() {
+		values = new ArrayList <Integer>();
+	}
+	
+	@Override
+	public String getName() {
+		return "alfa beta";
+	}
+
+	public static void main(String args[]) {
+	}
+
+	@Override
+	public Move nextMove(Board b) {
 		
-    	//zeby nie wybierac ciagle tego samego ruchu na poczatku gry, brany jest losowy ruch wsrod najlepszych
-    	List<Move> bestMoves = new ArrayList<Move>(); 
-    	bestMoves.add(moves.get(0));
-    	
-    	for (int i=1; i<moves.size(); ++i) {
-			tmp = minMove(moves.get(i), b, level);
-			if(tmp == bestValue)
+		int level = 5; // glebokosc przegladania
+		int result = maxMove(b, level, Integer.MIN_VALUE, Integer.MAX_VALUE, true);	
+
+		return getOneOfBest(b, result);
+	}
+	
+	/**
+	 * Funkcja zwraca losowy sposrod najlepszych wynikow
+	 */
+	public Move getOneOfBest(Board b, int result) {
+		List<Move> moves = b.getMovesFor(getColor());
+		
+		List<Move> bestMoves = new ArrayList<Move>();
+		for (int i=0; i<values.size(); ++i) {
+			if(values.get(i) == result)
 				bestMoves.add(moves.get(i));
-			else if (tmp > bestValue) {
-				bestMoves.clear();
-				bestMoves.add(moves.get(i));
-				bestValue = tmp;
-    		}
 		}
-    	
-        return bestMoves.get(random.nextInt(bestMoves.size()));
-    }
-    
-    /** 
-     * Funkcja zwraca ocene ruchu gracza min
-     */
-    public int minMove(Move move, Board board, int level) {
-    	Board b = board.clone();
-    	b.doMove(move);
-    	List<Move> moves = b.getMovesFor(getOpponent(getColor()));
-    	
-    	//jesli nie ma juz pionkow
-    	if (moves.size() == 0)
-    		if(this.count(getColor(), board) == 0)
-    			return Integer.MIN_VALUE;
-    		else
-    			return Integer.MAX_VALUE;
-    	
-    	int tmp, 
-    		bestValue;
-    	
-    	//szukanie minimalnej wartosci wsrod ruchow gracza max
-    	if (level > 0) { 
-    		bestValue = maxMove(moves.get(0), b, level-1);
-    		for (int i=1; i<moves.size(); ++i) {
-    			tmp = maxMove(moves.get(i), b, level-1);
-    			if (tmp < bestValue) {
-        			bestValue = tmp;
-        		}
-    		}
-    	}
-    	else { //jesli to juz ostatni poziom przeszukiwania
-        	bestValue = this.evaluateMove(moves.get(0), b);
-        	for (int i=1; i<moves.size(); ++i) {
-        		tmp = this.evaluateMove(moves.get(i), b);
-        		if (tmp < bestValue) {
-        			bestValue = tmp;
-        		}
-        	}
-    	}
-    	return bestValue;
-    }
-    
-    
-    /** 
-     * Funkcja zwraca ocene ruchu gracza max
-     */
-    public int maxMove(Move move, Board board, int level) {
-    	Board b = board.clone();
-    	b.doMove(move);
-    	List<Move> moves = b.getMovesFor(getColor());
-    	    	
-    	int tmp, 
-    		bestValue;
-    	
-    	//jesli nie ma juz pionkow
-    	if (moves.size() == 0)
-    		if(this.count(getColor(), board) == 0)
-    			return Integer.MIN_VALUE;
-    		else
-    			return Integer.MAX_VALUE;
-    	
-    	
-    	//szukanie maksymalnej wartosci wsrod ruchow gracza min
-    	if (level > 0) { 
-    		bestValue = minMove(moves.get(0), b, level-1);
-    		for (int i=1; i<moves.size(); ++i) {
-    			tmp = minMove(moves.get(i), b, level-1);
-    			if (tmp > bestValue) {
-        			bestValue = tmp;
-        		}
-    		}
-    	}
-    	else { //jesli to juz ostatni poziom przeszukiwania
-        	bestValue = this.evaluateMove(moves.get(0), b);
-        	for (int i=1; i<moves.size(); ++i) {
-        		tmp = this.evaluateMove(moves.get(i), b);
-        		if (tmp > bestValue) {
-        			bestValue = tmp;
-        		}
-        	}
-    	}
-    	return bestValue;
-       }
-    
-    /**
-     * Zwraca liczbe pionkow na planszy w danym kolorze
-     */
-    public int count(Color c, Board b) {
-    	int amount = 0;
-    	int sizeOfBoard = b.getSize();
-    	for (int x=0; x<sizeOfBoard; ++x)
-    		for (int y=0; y<sizeOfBoard; ++y)
-    			if (b.getState(x,y) == c)
-    				++amount;
-    	return amount;
-    }
-    
-    /** 
-     * Funkcja zwraca liczbę zbitych pionow 
-     * gracza o okreslonym kolorze w danym ruchu
-     */
-    public int lostPawns(Move move, Board board, Color color) {
-    	
-    	int beforeMove = this.count(color, board);
-    	
-    	Board b = board.clone(); 
-    	b.doMove(move);
-    	
-    	int afterMove = this.count(color, b);
-    	
-    	return beforeMove - afterMove;
-    }
-    
-    /**
-     * Funkcja zwraca roznice pomiedzy iloscia zbitych pionkow
-     * przeciwnika i sztucznej inteligencji
-     */
-    public int evaluateMove(Move move, Board board) {
-    	int mySpanked = lostPawns(move, board, getColor());
-    	int opponentSpanked = lostPawns(move, board, getOpponent(getColor()));    	
-    	return opponentSpanked - mySpanked;
-    }
-    
+		
+		return bestMoves.get(random.nextInt(bestMoves.size()));
+	}
+	
+
+	/**
+	 * Ruch gracza max
+	 */
+	public int maxMove(Board board, int level, int alpha, int beta, boolean initial) {
+		List<Move> moves = board.getMovesFor(getColor());
+		
+		int value;
+		
+		if(initial == true) 
+			values.clear();
+		
+		// jesli nie ma juz pionkow
+		if (moves.size() == 0)
+			if (this.count(getColor(), board) == 0) {
+				return Integer.MIN_VALUE;
+			}	
+			else {
+				return Integer.MAX_VALUE;
+			}
+
+		// szukanie maksymalnej wartosci wsrod ruchow gracza min
+		if (level > 0) {
+			for (int i = 0; i < moves.size(); ++i) {
+				Board b = board.clone();
+				b.doMove(moves.get(i));
+				
+				value = minMove(b, level - 1, alpha, beta);
+				alpha = max(value, alpha);
+				
+				if(initial == true) {
+					this.values.add(value);
+				}
+				
+				if (alpha >= beta) //odciecie beta
+					return beta;
+			}
+		} else { // jesli to juz ostatni poziom przeszukiwania
+			for (int i = 0; i < moves.size(); ++i) {
+				Board b = board.clone();
+				b.doMove(moves.get(i));
+								
+				value = this.evaluateMove(board, b);
+				alpha = max(value, alpha);
+
+				if (alpha >= beta)
+					return beta;
+			}
+		}
+		return alpha;
+	}
+	
+	/**
+	 * Ruch gracza min
+	 */
+	public int minMove(Board board, int level, int alpha, int beta) {
+		List<Move> moves = board.getMovesFor(getOpponent(getColor()));
+		
+		// jesli nie ma juz pionkow
+		if (moves.size() == 0)
+			if (this.count(getColor(), board) == 0) {
+				return Integer.MIN_VALUE;
+			}	
+			else {
+				return Integer.MAX_VALUE;
+			}
+		int value;
+
+		// szukanie minimalnej wartosci wsrod ruchow gracza max
+		if (level > 0) {
+			for (int i = 0; i < moves.size(); ++i) {
+				Board b = board.clone();
+				b.doMove(moves.get(i));
+				
+				value = maxMove(b, level - 1, alpha, beta, false);
+				beta = min(value, beta);
+				
+				if (alpha >= beta)
+					return alpha;
+			}
+		} else { // jesli to juz ostatni poziom przeszukiwania
+			for (int i = 0; i < moves.size(); ++i) {
+				Board b = board.clone();
+				b.doMove(moves.get(i));
+				
+				value = this.evaluateMove(board, b);
+				beta = min(value, beta);
+				
+				if (alpha >= beta)
+					return alpha;
+			}
+		}
+		return beta;
+	}
+
+	/**
+	 * Zwraca liczbe pionkow na planszy w danym kolorze
+	 */
+	public int count(Color c, Board b) {
+		int amount = 0;
+		int sizeOfBoard = b.getSize();
+		for (int x = 0; x < sizeOfBoard; ++x)
+			for (int y = 0; y < sizeOfBoard; ++y)
+				if (b.getState(x, y) == c)
+					++amount;
+		return amount;
+	}
+
+	/**
+	 * Funkcja zwraca liczbę zbitych pionow gracza o okreslonym kolorze w danym
+	 * ruchu
+	 */
+	public int lostPawns(Board board1, Board board2, Color color) {
+		int beforeMove = this.count(color, board1);
+		int afterMove = this.count(color, board2);
+
+		return beforeMove - afterMove;
+	}
+
+	/**
+	 * Funkcja zwraca roznice pomiedzy iloscia zbitych pionkow przeciwnika i
+	 * sztucznej inteligencji
+	 */
+	public int evaluateMove(Board board1, Board board2) {
+		int myLost = lostPawns(board1, board2, getColor());
+		int opponentLost = lostPawns(board1, board2, getOpponent(getColor()));
+		return opponentLost - myLost;
+	}
+
+	public int min(int a, int b) {
+		if (a < b)
+			return a;
+		return b;
+	}
+
+	public int max(int a, int b) {
+		if (a > b)
+			return a;
+		return b;
+	}
+
 }
